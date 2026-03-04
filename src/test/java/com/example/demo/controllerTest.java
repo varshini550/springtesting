67fd1cce3cd64faa4d3bc4efae942c10d0c99235
varshini550 <23mh1a0510@acoe.edu.in>
2026-03-04 11:34:53 +0530

    1

--- src/test/java/com/example/demo/controllerTest.java
+++ src/test/java/com/example/demo/controllerTest.java
@@ -2,17 +2,14 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class controllerTest {
-	@Autowired
-	controller c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
+
+    @Test
+    void test() {
+        controller c = new controller();
+        int result = c.addCSE(2, 3);
+        assertEquals(5, result);
+    }
 
 }
