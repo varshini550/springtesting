ad787287874a5bcbf55846e94e73091bcc1847a1
varshini550 <23mh1a0510@acoe.edu.in>
2026-03-04 11:37:50 +0530

    ok

--- Project12Application.java
+++ Project12Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Project12Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Project12Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Project12Application.class, args);
+    }
 
 }
